e7d31b8742a4d4a6bc11ea98f2017de77be7f68b
mehboussada <mehboussada@exoplatform.com>
2018-12-07 16:50:59 +0100

    Implement rest service

---
+++ src/main/java/com/tuto/RestService.java
@@ -0,0 +1,29 @@
+package com.tuto;
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONObject;
+
+
+@Path("/rest")
+public class RestService {
+	
+		@GET
+	  @Path("/test")
+	@Produces(MediaType.APPLICATION_JSON)
+	  public Response isRegistred() throws Exception {
+	    
+		JSONObject jsonObject = new JSONObject();
+	   jsonObject.put("registred", true);
+
+	    return Response.ok(jsonObject.toString(), MediaType.APPLICATION_JSON).build();
+	  }
+
+
+}
